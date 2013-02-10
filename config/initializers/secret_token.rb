3ac8cb85d123ee4cb1010c90feda4bd893aef020
luke_rasmussen <luke.rasmussen@gmail.com>
2013-02-09 21:54:03 -0600

    Generated application - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiClient::Application.config.secret_token = '40672b472ec1739e095d5621777c812234cd38d03c579ce944144030d5675efbceb6a1bd7068cbc158d17c3fd34c2be7d20a9ed1c96e6126ad40b8e41c3907b5'
