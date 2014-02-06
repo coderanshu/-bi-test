54aaea85892ce129224bbb51ac88a833d2d3ef59
Luke Rasmussen <luke.rasmussen@gmail.com>
2014-02-05 21:31:07 -0600

    Fixed postgres error

--- app/models/body_system.rb
+++ app/models/body_system.rb
@@ -13,7 +13,7 @@ class BodySystem < ActiveRecord::Base
   has_many :alerts
   attr_accessible :name, :order
 
-  default_scope order('body_systems.[order]')
+  default_scope order('body_systems."order"')
 
   def code
     name.downcase.gsub(/\s/, '_')
