b216b6e241e523bc0bad1dab08e95a52fa8002bc
Luke Rasmussen <luke.rasmussen@gmail.com>
2014-04-15 21:01:52 -0500

    Added more terms to our lookup list

--- app/assets/javascripts/observations.js
+++ app/assets/javascripts/observations.js
@@ -10,7 +10,26 @@ var GlobalVocabList = [
   {label:"pH", value:{code_system:"LOINC", code:"2746-6", name:"pH"}},
   {label:"paO2", value:{code_system:"LOINC", code:"3148-4", name:"paO2"}},
   {label:"paCO2", value:{code_system:"LOINC", code:"33022-5", name:"paCO2"}},
-  {label:"hemoglobin", value:{code_system:"LOINC", code:"726-0", name:"hemoglobin"}}
+  {label:"hemoglobin", value:{code_system:"LOINC", code:"726-0", name:"hemoglobin"}},
 
-  //
+  // Vital signs
+  {label:"Systolic blood pressure", value:{code_system:"LOINC", code:"8480-6", name:"Systolic blood pressure"}},
+  {label:"diastolic blood pressure", value:{code_system:"LOINC", code:"8462-4", name:"diastolic blood pressure"}},
+  {label:"heart rate", value:{code_system:"LOINC", code:"LP32063-7", name:"heart rate"}},
+  {label:"temperature (F)", value:{code_system:"LOINC", code:"LP29701-7", name:"temperature", units:"F"}},
+  {label:"body weight (lbs)", value:{code_system:"LOINC", code:"3141-9", name:"body weight", units:"lbs"}},
+  {label:"height (in)", value:{code_system:"LOINC", code:"LP64598-3", name:"height", units:"in"}},
+
+  // Ventilator setting
+  {label:"tidal volume", value:{code_system:"LOINC", code:"20112-9", name:"tidal volume"}},
+  {label:"ventilator set rate", value:{code_system:"LOINC", code:"19834-1", name:"ventilator set rate"}},
+  {label:"plateau pressure", value:{code_system:"LOINC", code:"LP94729-8", name:"plateau pressure"}},
+  {label:"fraction of inspired oxygen", value:{code_system:"LOINC", code:"19994-3", name:"fraction of inspired oxygen"}},
+
+  // Nursing documentation
+  {label:"Glasgow Coma Scale", value:{code_system:"SNOMEDCT", code:"386557006", name:"Glasgow Coma Scale"}},
+  {label:"CAM-ICU", value:{code_system:"LOINC", code:"LP74647-6", name:"CAM-ICU"}},
+
+  // Medications
+  {label:"phenylephrine", value:{code_system:"RXNORM", code:"8163", name:"phenylephrine"}},
 ];
